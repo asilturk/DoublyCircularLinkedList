17910eacea5835e24423de2dd31fd082cb83446a
Burak Furkan Asilturk <furkan.asilturk@gmail.com>
2020-05-17 03:01:14 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
+
+void printFunction() {
+    double d = 12;
+    double c = 22;
+
+    cout << d * c << endl;
+}
